52d77d2569c4d7b7dab7c638401015b1eba7d264
Anwar <anwarmohammadi1390@gmail.com>
2024-10-28 13:51:33 +0430

    some chages

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At culpa
+        distinctio in, tenetur perspiciatis repellendus hic minima eos ex odit
+        officiis pariatur deleniti dicta praesentium quis similique incidunt
+        soluta neque!
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
