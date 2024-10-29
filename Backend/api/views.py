9b02824fa74280a008f9c93020878f0540501532
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-10-29 16:16:34 +0430

    Order is is done with react

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+# views.py
+from rest_framework import viewsets
+from .models import Category, Order
+from .serializers import CategorySerializer, OrderSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -36,17 +36,21 @@ THIRD_PARTY_APPS = [
     "djoser",
     "rest_framework_simplejwt",
     'drf_yasg',
+    'corsheaders',
 ]
 
 LOCAL_APPS = [
     "accounts.apps.AccountsConfig",  
     "profiles.apps.ProfilesConfig",
+    "api.apps.ApiConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Add this line
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -177,3 +181,9 @@ DJOSER = {
         "user_delete": "djoser.serializers.UserDeleteSerializer",
     },
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Your React app's URL
+]
+CORS_ALLOW_ALL_ORIGINS = True
