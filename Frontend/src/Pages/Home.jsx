fe869e0fdd44c8181884b75f01aebf78baeedf93
Anwar <anwarmohammadi1390@gmail.com>
2024-10-30 15:56:25 +0430

    a little changes

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Services from "../Components/Services/Services";
-import Blog from "../Components/Blog/Blog";
+
 import About from "../Components/About/About";
 
 const Home = () => {
