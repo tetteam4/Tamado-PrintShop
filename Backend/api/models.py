66f03be1835a20790e2892d99ecf4037092d3825
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-10-30 15:37:18 +0430

    reception is done

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,30 +1,44 @@
+import datetime
 from ast import mod
+
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-
-import datetime
 
 
 class Category(models.Model):
-    name = models.CharField(_("Category Name :"),max_length=255)
+    name = models.CharField(_("Category Name :"), max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self) -> str:
         return self.name
+
     class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 
 class Order(models.Model):
     User = get_user_model()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Customer_name = models.CharField(_("Customer Name :"),max_length=255)
+    Customer_name = models.CharField(_("Customer Name :"), max_length=255)
     description = models.TextField()
     category = models.ManyToManyField(Category)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    
-    
+
+
+class Reception(models.Model):
+    User = get_user_model()
+    designer = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer_name = models.CharField(_("Customer Name :"), max_length=255)
+    order_name = models.ForeignKey(Order, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.0)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.designer.first_name
